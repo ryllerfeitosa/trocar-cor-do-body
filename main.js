314fb7d230d26ad13fd41ef0984c38f8e2de6367
RILLER DA COSTA FEITOSA <25203652@CFP127ALUNO.LOCAL>
2026-02-02 11:25:28 -0300

    projeto com alteração

--- main.js
+++ main.js
@@ -2,7 +2,16 @@
 
 const botaoTrocarCor = document.getElementById('trocar-cor')
 function trocarCor(){
-    const cor = document.getElementById('cor').value
+    let cor = document.getElementById('cor').value
+    if(cor == "azul"){
+        cor = "blue"
+    }else if(cor == "vermelho"){
+        cor = "red"
+    }else if(cor == "amarelo"){
+        cor = "yellow"
+    }else if(cor == "verde"){
+        cor = "green"
+    }
     document.documentElement.style.setProperty('--cor--bg', cor)
 }     
 botaoTrocarCor.addEventListener('click', trocarCor)
